960f1775b7a95ac1b7711d543a6000704962b9a5
Software-Development-of-Better-Tomorrow <s.d.of.b.t@gmail.com>
2017-09-10 01:42:56 +0200

    S-D-of-B-T moving to .org

--- clever_code/bio/php/contentSaver.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header("Content-type: text/plain");
-
-    $rawDataToSaveToFile = $_POST['contentRawData'];
-    $filePath = $_POST['contentRawDataStorageFilePath'];
-    
-    file_put_contents($filePath, $rawDataToSaveToFile, LOCK_EX);
-?>
